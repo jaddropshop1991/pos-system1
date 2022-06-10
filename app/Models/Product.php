8557b6bd57ea88e815aff7eb510275c768709b19
jaddropshop1991 <101974310+jaddropshop1991@users.noreply.github.com>
2022-06-10 14:23:10 +0300

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-public $keyType = ‘string’;
+
     protected $fillable =[
         'category_id','product_name','product_code',
         'root','buying_price','selling_price',
