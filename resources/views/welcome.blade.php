3b8ca8a70c7f2b88bc8bc6d5d6ad98805240cf39
jaddropshop1991 <101974310+jaddropshop1991@users.noreply.github.com>
2022-06-10 12:53:19 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,7 @@
   <link href="{{  asset('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
   <link href="{{  asset('backend/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
   <link href="{{  asset('backend/css/ruang-admin.min.css')}}" rel="stylesheet">
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 </head>
 
 <body id="page-top">
