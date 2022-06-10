532afea1d5771f6db87ddd3dc95a73311176f83c
jaddropshop1991 <101974310+jaddropshop1991@users.noreply.github.com>
2022-06-10 15:10:20 +0300

    Update POS.php

--- app/Models/POS.php
+++ app/Models/POS.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 class POS extends Model
 {
     use HasFactory;
-
+$keyType='string';
     protected $fillable =[
         'pro_id','pro_name','pro_quantity',
         'product_price','sub_total'
